1a383a64bf856a2c59777fdd9b64616cf73019e2
Mateo Castreuil <mateo.castreuil.01@student.henallux.be>
2024-12-18 00:08:39 +0100

    Adding documents from folder if not db

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
-from sqlmodel import SQLModel, create_engine, Session
 import os
+from sqlmodel import SQLModel, create_engine, Session, select
+from app.models import Document
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dev.db")
 engine = create_engine(DATABASE_URL)
+
+DOCUMENT_STORAGE_PATH = os.getenv("DOCUMENT_STORAGE_PATH", "./documents")
+os.makedirs(DOCUMENT_STORAGE_PATH, exist_ok=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
@@ -10,3 +14,14 @@ def create_db_and_tables():
 def get_session():
     with Session(engine) as session:
         yield session
+
+def add_existing_documents():
+    with Session(engine) as session:
+        for file_name in os.listdir(DOCUMENT_STORAGE_PATH):
+            if not session.exec(select(Document).where(Document.filename == file_name)).first():
+                document = Document(filename=file_name, path=os.path.join(DOCUMENT_STORAGE_PATH, file_name))
+                session.add(document)
+                session.commit()
+                session.refresh(document)
+
+    session.close()
