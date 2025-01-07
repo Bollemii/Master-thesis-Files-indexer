9cfcf08e6982a55b127496c3ed9a6e566c66411c
Mateo Castreuil <mateo.castreuil.01@student.henallux.be>
2025-01-07 19:40:54 +0100

    Adding CORS policy

---
+++ backend/app/main.py
@@ -0,0 +1,36 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+# from app.TopicModeling import topic_modeling_v2
+from app.routers import documents, users
+from app.database import create_db_and_tables, add_existing_documents
+
+# Initialize database
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    add_existing_documents()
+    yield
+
+# Initialize FastAPI
+app = FastAPI(title="Document Processing API", lifespan=lifespan)
+
+origins = [
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(documents.router)
+# app.include_router(users.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
